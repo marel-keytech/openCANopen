89878bcd46e21e4a119be25d8ba169130ce15052
Andri Yngvason <andri.yngvason@marel.com>
2014-12-18 13:46:58 +0000

    socketcan: add utility functions to open connections and apply filters

--- inc/socketcan.h
+++ inc/socketcan.h
@@ -8,6 +8,11 @@
 
 void socketcan_make_slave_filters(struct can_filter* filters, int nodeid);
 void socketcan_make_master_filters(struct can_filter* filters, int nodeid);
+int socketcan_open(const char* iface);
+int socketcan_apply_filters(int fd, struct can_filter* filters, int n);
+
+int socketcan_open_slave(const char* iface, int nodeid);
+int socketcan_open_master(const char* iface, int nodeid);
 
 #endif /* _CANOPEN_SOCKETCAN_H */
 
--- src/socketcan.c
+++ src/socketcan.c
@@ -1,4 +1,11 @@
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <unistd.h>
 #include <linux/can.h>
+#include <linux/can/raw.h>
 #include <assert.h>
 
 #include "canopen.h"
@@ -7,7 +14,7 @@
 static void set_sff_mask(struct can_filter* f, int len)
 {
 	int i;
-	for(i = 0; i < len; ++i)
+	for (i = 0; i < len; ++i)
 		f[i].can_mask = CAN_SFF_MASK;
 }
 
@@ -43,5 +50,77 @@ void socketcan_make_master_filters(struct can_filter* f, int nodeid)
 	f[7].can_id = R_TPDO4 + nodeid;
 	f[8].can_id = R_TSDO + nodeid;
 	f[9].can_id = R_HEARTBEAT + nodeid;
+}
+
+int socketcan_open(const char* iface)
+{
+	int fd;
+	struct sockaddr_can addr;
+	struct ifreq ifr;
+
+	fd = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+	if (fd < 0)
+		return -1;
+
+	memset(&ifr, 0, sizeof(ifr));
+	strcpy(ifr.ifr_name, iface);
+	if (ioctl(fd, SIOCGIFINDEX, &ifr) < 0)
+		goto error;
+
+	memset(&addr, 0, sizeof(addr));
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+	if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+		goto error;
+
+	return fd;
+
+error:
+	close(fd);
+	return -1;
+}
+
+int socketcan_apply_filters(int fd, struct can_filter* filters, int n)
+{
+	return setsockopt(fd, SOL_CAN_RAW, CAN_RAW_FILTER, filters,
+			  n*sizeof(struct can_filter));
+}
+
+int socketcan_open_slave(const char* iface, int nodeid)
+{
+	struct can_filter filters[CANOPEN_SLAVE_FILTER_LENGTH];
+
+	int fd = socketcan_open(iface);
+	if (fd < 0)
+		return -1;
+
+	socketcan_make_slave_filters(filters, nodeid);
+
+	if (socketcan_apply_filters(fd, filters,
+				    CANOPEN_SLAVE_FILTER_LENGTH) < 0) {
+		close(fd);
+		return -1;
+	}
+
+	return fd;
+}
+
+int socketcan_open_master(const char* iface, int nodeid)
+{
+	struct can_filter filters[CANOPEN_MASTER_FILTER_LENGTH];
+
+	int fd = socketcan_open(iface);
+	if (fd < 0)
+		return -1;
+
+	socketcan_make_master_filters(filters, nodeid);
+
+	if (socketcan_apply_filters(fd, filters,
+				    CANOPEN_MASTER_FILTER_LENGTH) < 0) {
+		close(fd);
+		return -1;
+	}
+
+	return fd;
 }
 
