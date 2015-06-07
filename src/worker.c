03500a0ab0ee5036e606c7f53f90a2571df86f67
Andri Yngvason <andri.yngvason@marel.com>
2015-06-07 16:28:43 +0000

    worker: add suspend mechanism

--- src/worker.c
+++ src/worker.c
@@ -5,6 +5,10 @@
 static struct prioq job_queue_;
 static int nthreads_;
 static pthread_t* threads_;
+static pthread_mutex_t mutex_;
+static pthread_cond_t cond_;
+static unsigned long njobs_;
+static int please_exit_;
 
 struct job {
 	void (*fn)(void*);
@@ -28,23 +32,48 @@ static void job_del(struct job* job)
 	free(job);
 }
 
+static inline void lock()
+{
+	pthread_mutex_lock(&mutex_);
+}
+
+static inline void unlock()
+{
+	pthread_mutex_unlock(&mutex_);
+}
+
+static void wait_for_jobs()
+{
+	lock();
+	while (!please_exit_ && njobs_ > 0)
+		pthread_cond_wait(&cond_, &mutex_);
+	unlock();
+}
+
+static void decrement_njobs()
+{
+	lock();
+	njobs_ -= njobs_ > 0 ? 1 : 0;
+	unlock();
+}
+
 static void* thread_fn(void* unused)
 {
 	(void)unused;
 	struct prioq_elem elem;
 
 	while (1) {
-		prioq_wait(&job_queue_);
+		wait_for_jobs();
 
-		pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
-		while (1) {
-			if (prioq_pop(&job_queue_, &elem)) {
-				struct job* job = (struct job*)elem.data;
-				job->fn(job->context);
-				job_del(job);
-			}
+		if (please_exit_)
+			break;
+
+		while (prioq_pop(&job_queue_, &elem)) {
+			decrement_njobs();
+			struct job* job = (struct job*)elem.data;
+			job->fn(job->context);
+			job_del(job);
 		}
-		pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
 	}
 
 	return NULL;
@@ -69,8 +98,14 @@ static int start_threads(size_t stacksize)
 
 int worker_init(int nthreads, size_t qsize, size_t stacksize)
 {
+	njobs_ = 0;
+	please_exit_ = 0;
+
 	if (prioq_init(&job_queue_, qsize) < 0)
 		return -1;
+
+	pthread_mutex_init(&mutex_, NULL);
+	pthread_cond_init(&cond_, NULL);
 
 	nthreads_ = nthreads;
 	threads_ = calloc(nthreads_, sizeof(pthread_t));
@@ -85,6 +120,8 @@ int worker_init(int nthreads, size_t qsize, size_t stacksize)
 thread_start_failure:
 	free(threads_);
 thread_alloc_failure:
+	pthread_cond_destroy(&cond_);
+	pthread_mutex_destroy(&mutex_);
 	prioq_clear(&job_queue_);
 	return -1;
 }
@@ -99,12 +136,11 @@ static void clear_jobs()
 
 static void reap_threads()
 {
-	int i;
-	for (i = 0; i < nthreads_; ++i)
-	{
-		pthread_cancel(threads_[i]);
+	pthread_cond_broadcast(&cond_);
+	please_exit_ = 1;
+
+	for (int i = 0; i < nthreads_; ++i)
 		pthread_join(threads_[i], NULL);
-	}
 }
 
 void worker_deinit()
@@ -113,6 +149,16 @@ void worker_deinit()
 	free(threads_);
 	clear_jobs();
 	prioq_clear(&job_queue_);
+	pthread_cond_destroy(&cond_);
+	pthread_mutex_destroy(&mutex_);
+}
+
+static void increment_njobs()
+{
+	lock();
+	++njobs_;
+	pthread_cond_broadcast(&cond_);
+	unlock();
 }
 
 int worker_add_job(unsigned long priority, void (*job_fn)(void*), void* context)
@@ -123,6 +169,8 @@ int worker_add_job(unsigned long priority, void (*job_fn)(void*), void* context)
 
 	if (prioq_insert(&job_queue_, priority, job) < 0)
 		return -1;
+
+	increment_njobs();
 
 	return 0;
 }
