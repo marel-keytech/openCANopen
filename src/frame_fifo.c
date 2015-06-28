eb29f3a8c0c1992f5511010eabd32e196ac969d3
Andri Yngvason <any1@hi.is>
2015-06-28 17:11:50 +0000

    frame_fifo: use thread-utils for conditional blocking

--- src/frame_fifo.c
+++ src/frame_fifo.c
@@ -1,10 +1,7 @@
 #include <errno.h>
 #include <time.h>
 #include "frame_fifo.h"
-
-#define nSEC_IN_SEC 1000000000ULL
-
-#define msec_to_nsec(t) ((t) * 1000000ULL)
+#include "thread-utils.h"
 
 int frame_fifo_init(struct frame_fifo* self)
 {
@@ -54,75 +51,25 @@ void frame_fifo_enqueue(struct frame_fifo* self, const struct can_frame* frame)
 	frame_fifo__unlock(self);
 }
 
-static inline uint64_t timespec_to_ns(struct timespec* ts)
-{
-	return ts->tv_sec * nSEC_IN_SEC + ts->tv_nsec;
-}
-
-static inline struct timespec ns_to_timespec(uint64_t ns)
-{
-	struct timespec ts;
-	ts.tv_nsec = ns % nSEC_IN_SEC;
-	ts.tv_sec = ns / nSEC_IN_SEC;
-	return ts;
-}
-
-static inline void add_to_timespec(struct timespec* ts, uint64_t addition)
-{
-	*ts = ns_to_timespec(timespec_to_ns(ts) + addition);
-}
-
-static int block_while_empty(struct frame_fifo* self, int timeout)
-{
-	struct timespec deadline;
-
-	if (timeout < 0) {
-		while (self->count == 0)
-			pthread_cond_wait(&self->suspend_cond_,
-					  &self->mutex_);
-	} else {
-		clock_gettime(CLOCK_REALTIME, &deadline);
-		add_to_timespec(&deadline, msec_to_nsec(timeout));
-
-		while (self->count == 0)
-			if (pthread_cond_timedwait(&self->suspend_cond_,
-						   &self->mutex_,
-						   &deadline) == ETIMEDOUT)
-				if (self->count == 0)
-					return ETIMEDOUT;
-	}
-
-	return 0;
-}
-
 int frame_fifo_dequeue(struct frame_fifo* self, struct can_frame* frame,
 		       int timeout)
 {
 	frame_fifo__lock(self);
 
-	if (timeout == 0) {
-		if (self->count == 0) {
-			errno = EAGAIN;
-			goto failure;
-		}
-	} else {
-		if (block_while_empty(self, timeout) == ETIMEDOUT) {
-			errno = ETIMEDOUT;
-			goto failure;
-		}
-	}
+	int rc = block_thread_while_empty(&self->suspend_cond_, &self->mutex_,
+					  timeout, self->count == 0);
+	if (rc < 0)
+		goto done;
 
 	*frame = *frame_fifo_head(self);
 
 	advance_start(self);
 	--self->count;
 
+	rc = 1;
+done:
 	frame_fifo__unlock(self);
-	return 1;
-
-failure:
-	frame_fifo__unlock(self);
-	return -1;
+	return rc;
 }
 
 void frame_fifo_flush(struct frame_fifo* self)
