9e89efab7d65fc28229898377f3b83267a8ae897
Andri Yngvason <andri.yngvason@marel.com>
2015-09-15 11:14:32 +0000

    sdo_req_test: update to reflect current implementation

--- tst/sdo_req_test.c
+++ tst/sdo_req_test.c
@@ -4,16 +4,19 @@
 
 DEFINE_FFF_GLOBALS;
 
+FAKE_VOID_FUNC(mloop_iterate, struct mloop*);
 FAKE_VALUE_FUNC(struct mloop*, mloop_default);
-FAKE_VALUE_FUNC(struct mloop_async*, mloop_async_new);
-FAKE_VALUE_FUNC(int, mloop_start_async, struct mloop*, struct mloop_async*);
-FAKE_VALUE_FUNC(int, mloop_async_unref, struct mloop_async*);
-FAKE_VOID_FUNC(mloop_async_set_context, struct mloop_async*, void*,
+FAKE_VALUE_FUNC(struct mloop_idle*, mloop_idle_new);
+FAKE_VALUE_FUNC(int, mloop_start_idle, struct mloop*, struct mloop_idle*);
+FAKE_VALUE_FUNC(int, mloop_idle_unref, struct mloop_idle*);
+FAKE_VOID_FUNC(mloop_idle_set_context, struct mloop_idle*, void*,
 	       mloop_free_fn);
-FAKE_VALUE_FUNC(void*, mloop_async_get_context, const struct mloop_async*);
-FAKE_VOID_FUNC(mloop_async_set_callback, struct mloop_async*, mloop_async_fn);
-FAKE_VOID_FUNC(mloop_async_set_priority, struct mloop_async*, unsigned long);
+FAKE_VALUE_FUNC(void*, mloop_idle_get_context, const struct mloop_idle*);
+FAKE_VOID_FUNC(mloop_idle_set_idle_fn, struct mloop_idle*, mloop_idle_fn);
+FAKE_VOID_FUNC(mloop_idle_set_cond_fn, struct mloop_idle*, mloop_idle_cond_fn);
+FAKE_VOID_FUNC(mloop_idle_set_priority, struct mloop_idle*, unsigned long);
 FAKE_VALUE_FUNC(int, sdo_async_init, struct sdo_async*, int, int);
+FAKE_VALUE_FUNC(int, sdo_async_stop, struct sdo_async*);
 FAKE_VOID_FUNC(sdo_async_destroy, struct sdo_async*);
 FAKE_VALUE_FUNC(int, sdo_async_start, struct sdo_async*,
 		const struct sdo_async_info*);
@@ -50,8 +53,13 @@ static int test_req_queue_init_destroy()
 	RESET_FAKE(sdo_async_init);
 	sdo_async_init_fake.return_val = 0;
 
+	struct mloop_idle* idle = (void*)0xdeadbeef;
+
+	RESET_FAKE(mloop_idle_new);
+	mloop_idle_new_fake.return_val = idle;
+
 	struct sdo_req_queue queue;
-	ASSERT_INT_EQ(0, sdo_req__queue_init(&queue, 4, 42, 10));
+	ASSERT_INT_EQ(0, sdo_req__queue_init(&queue, 4, 42, 10, 0));
 
 	ASSERT_INT_EQ(4, sdo_async_init_fake.arg1_val);
 	ASSERT_INT_EQ(42, sdo_async_init_fake.arg2_val);
@@ -68,7 +76,7 @@ static int test_req_queue_enqueue_dequeue()
 	sdo_async_init_fake.return_val = 0;
 
 	struct sdo_req_queue queue;
-	sdo_req__queue_init(&queue, 0, 0, 3);
+	sdo_req__queue_init(&queue, 0, 0, 3, 0);
 
 	struct sdo_req req[4];
 	memset(req, 0, sizeof(req));
@@ -87,11 +95,6 @@ static int test_req_queue_enqueue_dequeue()
 	ASSERT_PTR_EQ(&req[1], sdo_req_queue__dequeue(&queue));
 	ASSERT_PTR_EQ(&req[2], sdo_req_queue__dequeue(&queue));
 	ASSERT_PTR_EQ(NULL, sdo_req_queue__dequeue(&queue));
-
-	ASSERT_PTR_EQ(NULL, req[0].parent);
-	ASSERT_PTR_EQ(NULL, req[1].parent);
-	ASSERT_PTR_EQ(NULL, req[2].parent);
-	ASSERT_PTR_EQ(NULL, req[3].parent);
 
 	sdo_req__queue_destroy(&queue);
 	return 0;
