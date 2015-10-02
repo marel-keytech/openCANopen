9f867c1d78bec306b790f5e1801b3698b09e7992
Andri Yngvason <andri.yngvason@marel.com>
2015-10-02 11:28:13 +0000

    can-tcp: use linked list to route between sockets

--- src/can-tcp.c
+++ src/can-tcp.c
@@ -8,66 +8,117 @@
 #include <arpa/inet.h>
 #include <mloop.h>
 
+#include "sys/queue.h"
 #include "socketcan.h"
 #include "net-util.h"
+#include "sock.h"
 
 size_t strlcpy(char*, const char*, size_t);
 
-struct can_tcp__context {
-	int is_can;
-	int other_fd;
+struct can_tcp;
+
+struct can_tcp_entry {
+	struct sock sock;
+	struct can_tcp* parent;
+	LIST_ENTRY(can_tcp_entry) links;
 };
 
-struct can_tcp__server_context {
-	char iface[256];
+LIST_HEAD(can_tcp_list, can_tcp_entry);
+
+struct can_tcp {
+	struct can_tcp_list list;
+	size_t ref;
 };
+
+static struct can_tcp* can_tcp__new(void)
+{
+	struct can_tcp* self = malloc(sizeof(*self));
+	if (!self)
+		return NULL;
+
+	LIST_INIT(&self->list);
+	self->ref = 1;
+
+	return self;
+}
+
+static void can_tcp__free(struct can_tcp* self)
+{
+	free(self);
+}
+
+static void can_tcp__ref(struct can_tcp* self)
+{
+	++self->ref;
+}
+
+static void can_tcp__unref(struct can_tcp* self)
+{
+	if (--self->ref == 0)
+		can_tcp__free(self);
+}
+
+static void can_tcp__send_to_others(struct can_tcp_entry* entry,
+				    struct can_frame* cf)
+{
+	struct can_tcp* parent = entry->parent;
+	struct can_tcp_entry* elem = NULL;
+
+	LIST_FOREACH(elem, &parent->list, links)
+		if (elem != entry)
+			sock_send(&elem->sock, cf, 0);
+}
 
 static void can_tcp__forward_message(struct mloop_socket* socket)
 {
-	struct can_tcp__context* context = mloop_socket_get_context(socket);
-	assert(context);
-
-	int fd = mloop_socket_get_fd(socket);
-	int other_fd = context->other_fd;
-	int is_can = context->is_can;
-
 	struct can_frame cf;
+	struct can_tcp_entry* entry = mloop_socket_get_context(socket);
+	assert(entry);
 
-	if (net_read_frame(fd, &cf, 0) <= 0)
+	if (sock_recv(&entry->sock, &cf, 0) <= 0)
 		mloop_socket_stop(socket);
 
-	cf.can_id = is_can ? ntohl(cf.can_id) : htonl(cf.can_id);
+	can_tcp__send_to_others(entry, &cf);
+}
 
-	net_write_frame(other_fd, &cf, -1);
+static void can_tcp_entry__free(void* ptr)
+{
+	struct can_tcp_entry* entry = ptr;
+	LIST_REMOVE(entry, links);
+	sock_close(&entry->sock);
+	can_tcp__unref(entry->parent);
+	free(entry);
 }
 
 static struct mloop_socket*
-can_tcp__setup_forward(int from, int to, int is_from_can)
+can_tcp__add_entry(struct can_tcp* self, const struct sock* sock)
 {
-	struct can_tcp__context* context;
-
-	context = malloc(sizeof(*context));
-	if (!context)
+	struct can_tcp_entry* entry = malloc(sizeof(*entry));
+	if (!entry)
 		return NULL;
-
-	context->other_fd = to;
-	context->is_can = is_from_can;
 
 	struct mloop_socket* s = mloop_socket_new();
 	if (!s)
 		goto failure;
 
-	mloop_socket_set_context(s, context, free);
+	entry->parent = self;
+	entry->sock = *sock;
+	LIST_INSERT_HEAD(&self->list, entry, links);
+
+	mloop_socket_set_context(s, entry, can_tcp_entry__free);
 	mloop_socket_set_callback(s, can_tcp__forward_message);
-	mloop_socket_set_fd(s, from);
+	mloop_socket_set_fd(s, sock->fd);
 
 	int rc = mloop_start_socket(mloop_default(), s);
 	mloop_socket_unref(s);
 
+	if (rc >= 0)
+		can_tcp__ref(self);
+
 	return rc >= 0 ? s : NULL;
 
 failure:
-	free(context);
+	free(entry);
 	return NULL;
 }
 
@@ -137,12 +188,9 @@ failure:
 
 static void can_tcp__on_connection(struct mloop_socket* socket)
 {
-	struct can_tcp__server_context* context;
-	context = mloop_socket_get_context(socket);
-	assert(context);
-
 	int sfd = mloop_socket_get_fd(socket);
-	const char* iface = context->iface;
+	struct can_tcp* can_tcp = mloop_socket_get_context(socket);
+	assert(can_tcp);
 
 	int connfd = accept(sfd, NULL, 0);
 	if (connfd < 0) {
@@ -150,92 +198,126 @@ static void can_tcp__on_connection(struct mloop_socket* socket)
 		return;
 	}
 
-	int cfd = open_can(iface);
-	if (cfd < 0) {
-		perror("Could not open CAN interface");
-		goto can_failure;
-	}
+	struct sock sock = { .fd = connfd, .type = SOCK_TYPE_TCP };
 
-	struct mloop_socket* s1 = can_tcp__setup_forward(connfd, cfd, 0);
-	if (!s1) {
-		perror("Could not create mloop handler for connection");
-		goto s1_failure;
-	}
-
-	struct mloop_socket* s2 = can_tcp__setup_forward(cfd, connfd, 1);
-	if (!s2) {
+	struct mloop_socket* s = can_tcp__add_entry(can_tcp, &sock);
+	if (!s) {
 		perror("Could not create mloop handler for CAN interface");
-		goto s2_failure;
+		goto failure;
 	}
 
 	return;
 
-s2_failure:
-	mloop_socket_stop(s1);
-s1_failure:
-	close(cfd);
-can_failure:
+failure:
 	close(connfd);
+}
+
+struct mloop_socket* can_tcp__setup_server(int port)
+{
+	struct can_tcp* can_tcp = can_tcp__new();
+	if (!can_tcp) {
+		perror("ERROR");
+		return NULL;
+	}
+
+	int sfd = open_tcp_server(port);
+	if (sfd < 0) {
+		perror("Could not open tcp server");
+		goto server_failure;
+	}
+
+	struct mloop_socket* s = mloop_socket_new();
+	if (!s) {
+		perror("Could not create server socket handler");
+		goto handler_failure;
+	}
+
+	mloop_socket_set_context(s, can_tcp, (mloop_free_fn)can_tcp__unref);
+	mloop_socket_set_fd(s, sfd);
+	mloop_socket_set_callback(s, can_tcp__on_connection);
+
+	int rc = mloop_start_socket(mloop_default(), s);
+	if (rc < 0)
+		perror("Could not start server socket handler");
+
+	mloop_socket_unref(s);
+
+	return rc >= 0 ? s : NULL;
+
+handler_failure:
+	close(sfd);
+server_failure:
+	can_tcp__free(can_tcp);
+	return NULL;
 }
 
 __attribute__((visibility("default")))
 int can_tcp_bridge_server(const char* can, int port)
 {
-	int sfd = open_tcp_server(port);
-	if (sfd < 0)
+	struct sock cansock;
+
+	if (sock_open(&cansock, SOCK_TYPE_CAN, can) < 0) {
+		perror("Could not open CAN interface");
 		return -1;
+	}
 
-	struct mloop_socket* s = mloop_socket_new();
+	struct mloop_socket* s = can_tcp__setup_server(port);
 	if (!s)
-		goto handler_failure;
+		goto server_failure;
 
-	struct can_tcp__server_context* context;
-	context = malloc(sizeof(*context));
-	if (!context)
-		goto context_failure;
+	struct can_tcp* can_tcp = mloop_socket_get_context(s);
 
-	strlcpy(context->iface, can, sizeof(context->iface));
+	if (can_tcp__add_entry(can_tcp, &cansock) == NULL) {
+		perror("Could not create mloop handler for connection");
+		goto entry_failure;
+	}
 
-	mloop_socket_set_context(s, context, free);
-	mloop_socket_set_fd(s, sfd);
-	mloop_socket_set_callback(s, can_tcp__on_connection);
-	int rc = mloop_start_socket(mloop_default(), s);
-	mloop_socket_unref(s);
-	return rc;
+	return 0;
 
-context_failure:
-	mloop_socket_unref(s);
-handler_failure:
-	close(sfd);
+entry_failure:
+	mloop_socket_stop(s);
+server_failure:
+	sock_close(&cansock);
 	return -1;
 }
 
 __attribute__((visibility("default")))
 int can_tcp_bridge_client(const char* can, const char* address, int port)
 {
-	int cfd = open_can(can);
-	if (cfd < 0)
+	struct sock cansock;
+
+	struct can_tcp* can_tcp = can_tcp__new();
+	if (!can_tcp)
 		return -1;
+
+	if (sock_open(&cansock, SOCK_TYPE_CAN, can) < 0)
+		goto cansock_failure;
 
 	int connfd = can_tcp_open(address, port);
 	if (connfd < 0)
-		goto client_failure;
+		goto tcpsock_failure;
 
-	struct mloop_socket* s1 = can_tcp__setup_forward(connfd, cfd, 0);
+	struct sock connsock = { .fd = connfd, .type = SOCK_TYPE_TCP };
+
+	struct mloop_socket* s1 = can_tcp__add_entry(can_tcp, &cansock);
 	if (!s1)
 		goto s1_failure;
 
-	struct mloop_socket* s2 = can_tcp__setup_forward(cfd, connfd, 1);
+	struct mloop_socket* s2 = can_tcp__add_entry(can_tcp, &connsock);
 	if (!s2)
 		goto s2_failure;
+
+	can_tcp__unref(can_tcp);
 
 	return 0;
 
 s2_failure:
 	mloop_socket_stop(s1);
 s1_failure:
-	close(connfd);
-client_failure:
-	close(cfd);
+	sock_close(&connsock);
+tcpsock_failure:
+	sock_close(&cansock);
+cansock_failure:
+	can_tcp__free(can_tcp);
 	return -1;
 }
