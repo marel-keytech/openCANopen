df7b80f1a227a99648c652dd5c2bb881f46441df
Andri Yngvason <andri.yngvason@marel.com>
2015-10-26 10:40:09 +0000

    sdo: add error codes and strings

--- src/sdo_common.c
+++ src/sdo_common.c
@@ -25,20 +25,42 @@ const char* sdo_strerror(enum sdo_abort_code code)
 		return "SDO protocol timed out";
 	case SDO_ABORT_INVALID_CS:
 		return "Client/server command specifier not valid or unknown";
+	case SDO_ABORT_BLOCKSZ:
+		return "Invalid block size";
+	case SDO_ABORT_SEQNR:
+		return "Invalid sequence number";
+	case SDO_ABORT_CRCERR:
+		return "CRC error";
 	case SDO_ABORT_NOMEM:
 		return "Out of memory";
+	case SDO_ABORT_ACCESS:
+		return "Unsupported access";
+	case SDO_ABORT_RO:
+		return "Attempt to write a read only object";
+	case SDO_ABORT_WO:
+		return "Attempt to read a write only object";
 	case SDO_ABORT_NEXIST:
 		return "Object does not exist in the object dictionary";
+	case SDO_ABORT_NOPDO:
+		return "Object cannot be mapped to PDO";
+	case SDO_ABORT_PDOSZ:
+		return "The number and length of objects to be mapped would exceed PDO length";
+	case SDO_ABORT_PARCOMPAT:
+		return "General parameter incompatibility error";
+	case SDO_ABORT_DEVCOMPAT:
+		return "General internal incompatibility in device";
+	case SDO_ABORT_HWERROR:
+		return "Access failed due to a hardware error";
 	case SDO_ABORT_SIZE:
 		return "Data type does not match, length of service parameter does not match";
 	case SDO_ABORT_TOO_LONG:
 		return "Data type does not match, length of service parameter too high";
 	case SDO_ABORT_TOO_SHORT:
 		return "Data type does not match, length of service parameter too low";
-	case SDO_ABORT_RO:
-		return "Attempt to write a read only object";
-	case SDO_ABORT_WO:
-		return "Attempt to read a write only object";
+	case SDO_ABORT_SUBNEXIST:
+		return "Sub-index does not exist";
+	case SDO_ABORT_NVAL:
+		return "Invalid value for parameter";
 	case SDO_ABORT_GENERAL:
 		return "General error";
 	}
