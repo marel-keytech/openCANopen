marel-keytech/openCANopen

commit ea4c2f39e79d4659c402b0fe9a9a1ea293aefd7c

master: don't reset nodes that are already loading

src/master.c
@@ -811,7 +811,7 @@ static int handle_heartbeat(struct co_master_node* node,
 	/* This can happen if the CAN bus is disconnected but not the power to
 	 * the node. We reset communication to refresh the state.
 	 */
-	if (node->driver_type == CO_MASTER_DRIVER_NONE) {
+	if (node->driver_type == CO_MASTER_DRIVER_NONE && !node->is_loading) {
 		if (heartbeat_get_state(frame) != NMT_STATE_STOPPED)
 			co_net_send_nmt(&socket_, NMT_CS_RESET_COMMUNICATION,
 					nodeid);

commit 8c413466985d8b97390faad4a914d985c4bdfc62

master: fix possible crash during driver reload
This fixes a regression from commit ea4c2f39e79d4659c402b0fe9a9a1ea293aefd7c

The !is_loading condition was misplaced. It makes no sense to pass on a
heartbeat for a node that has no driver yet.

src/master.c
@@ -812,8 +812,9 @@ static int handle_heartbeat(struct co_master_node* node,
 	/* This can happen if the CAN bus is disconnected but not the power to
 	 * the node. We reset communication to refresh the state.
 	 */
-	if (node->driver_type == CO_MASTER_DRIVER_NONE && !node->is_loading) {
-		if (heartbeat_get_state(frame) != NMT_STATE_STOPPED)
+	if (node->driver_type == CO_MASTER_DRIVER_NONE) {
+		if (heartbeat_get_state(frame) != NMT_STATE_STOPPED
+		 && !node->is_loading)
 			co_net_send_nmt(&socket_, NMT_CS_RESET_COMMUNICATION,
 					nodeid);
 